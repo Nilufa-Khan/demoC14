f0e906f1dc26356a2d2fdadcd6b2a9b9add9b061
Nilufa Khan <khannilufa5@gmail.com>
2022-11-10 15:16:55 +0530

    Initial commit

---
+++ src/main/java/com/example/demoC14/DemoC14Application.java
@@ -0,0 +1,13 @@
+package com.example.demoC14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoC14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoC14Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demoC14/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.demoC14.controller;
+
+import com.example.demoC14.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userdata/v1")
+public class UserController {
+    UserService userService;
+    @Autowired
+    public UserController (UserService userService){
+        this.userService = userService;
+    }
+
+    public ResponseEntity<?> saveUser()
+}
