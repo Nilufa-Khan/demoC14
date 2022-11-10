2c64be9f75578eff21187a7802451d32427978ed
Nilufa Khan <khannilufa5@gmail.com>
2022-11-10 15:19:43 +0530

    Initial commit

--- src/main/java/com/example/demoC14/controller/UserController.java
+++ src/main/java/com/example/demoC14/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demoC14.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,9 @@ public class UserController {
     public UserController (UserService userService){
         this.userService = userService;
     }
-
-    public ResponseEntity<?> saveUser(User user){
+@PostMapping("/postuser")
+    public ResponseEntity<?> saveUser(@RequestBody  User user){
         return new ResponseEntity<>(userService.addUser(user), HttpStatus.CREATED);
     }
+
 }
